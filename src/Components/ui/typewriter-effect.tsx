9696c02fee5b68fa2e783b53d0d859560e373659
Nikhil chopra <nikhil0080012005@gmail.com>
2024-09-28 19:50:58 +0530

    color fixing for testing page

--- src/Components/ui/typewriter-effect.tsx
+++ src/Components/ui/typewriter-effect.tsx
@@ -53,7 +53,7 @@ export const TypewriterEffect = ({
               initial={{}}
               key={`char-${index}`}
               className={cn(
-                `dark:text-white text-black opacity-0 hidden`,
+                `dark:text-white text-white opacity-0 hidden`,
                 word.className
               )}
             >
@@ -116,7 +116,7 @@ export const TypewriterEffectSmooth = ({
           {word.text.map((char, index) => (
             <span
               key={`char-${index}`}
-              className={cn(`dark:text-white text-black`, word.className)}
+              className={cn(`dark:text-white text-white`, word.className)}
             >
               {char}
             </span>
